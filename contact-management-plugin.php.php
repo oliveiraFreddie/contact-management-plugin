772f70c1ed28e92277498eca2c00be744740f819
Frederico Oliveira <freddie.oliveira95@gmail.com>
2024-03-24 11:07:51 +0000

    first

---
+++ contact-management-plugin.php.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Contact Management Plugin
+Description: Plugin para gerenciar contatos no WordPress.
+Version: 1.0.0
+Author: Frederico Santos
+Author URI: https://frederico-santos.vercel.app/
+*/
+
